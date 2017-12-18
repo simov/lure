b28c744a4e618de8201f6b3096b8339daeeda4f5
simov <simeonvelichkov@gmail.com>
2017-12-18 14:12:18 +0200

    Get all github members

--- api/github.js
+++ api/github.js
@@ -14,10 +14,22 @@ module.exports = (config) => ({
         .auth(config[key].token)
         .request())),
 
-  users: ({key}) => github
-    .get('orgs/' + config[key].id + '/members')
-    .headers({'user-agent': 'lure'})
-    .auth(config[key].token)
-    .request()
-    .then(([res, body]) => ({active: body.length}))
+  users: ({key}) => new Promise((resolve, reject) => {
+    var active = 0
+    ;(function get (page) {
+      github
+        .get('orgs/' + config[key].id + '/members')
+        .qs({page})
+        .headers({'user-agent': 'lure'})
+        .auth(config[key].token)
+        .request()
+        .then(([res, body]) => {
+          active += body.length
+          ;(!body.length || body.length < 30)
+            ? resolve({active})
+            : get(++page)
+        })
+        .catch(reject)
+    })(1)
+  })
 })
